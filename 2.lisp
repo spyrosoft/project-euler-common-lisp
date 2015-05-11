391f794253b0aebb2746e444b2c74f24c5a496c4
Bennett Roesch <meta@spyrosoft.com>
2015-05-11 01:08:05 -0700

    Actually working solution to #2.

--- 2.lisp
+++ 2.lisp
@@ -1,9 +1,11 @@
 (defun fibonacci ( one-back two-back even-number-sum )
 	(let* ((current-number (+ one-back two-back)))
-		(if (= (mod current-number 2))
-				(setq even-number-sum (+ even-number-sum current-number)))
-		(if (> current-number 40)
-				even-number-sum
-				(fibonacci current-number one-back even-number-sum))))
+		(print current-number)
+		(cond ((< current-number 4000000)
+					 (if (= (mod current-number 2) 0)
+							 (setq even-number-sum (+ even-number-sum current-number)))
+					 (fibonacci current-number one-back even-number-sum))
+					(t
+					 even-number-sum))))
 
 (print (fibonacci 1 1 0))
