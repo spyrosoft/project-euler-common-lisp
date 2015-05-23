21886913bfedf7e0a428d30a84b00ade7e6699b3
Bennett Roesch <meta@spyrosoft.com>
2015-05-23 15:18:41 -0700

    Comment third and add fourth attempt.

--- 3.lisp
+++ 3.lisp
@@ -3,22 +3,42 @@
 
 (ql:quickload :iterate)
 
-; Third attempt:
+(defpackage :project-euler-problem-3
+	(:use :common-lisp :iterate))
 
-(defun generate-next-prime-number (current-prime-test prime-numbers)
-	(let ((prime-number))
-		(iter (for prime-number in prime-numbers)
-					(cond ((not (= (mod current-prime-test prime-number) 0))
-								 current-prime-test)
-								((generate-next-prime-number (1+ current-prime-test) prime-numbers))
-								))))
+(in-package :project-euler-problem-3)
+
+; Fourth attempt:
+
+(defun generate-primes-up-to-number (limit)
+	)
 
 (defun locate-largest-prime-factor (target-number)
-	(do ((prime-numbers
-				'(2)
-				(push prime-numbers (generate-next-prime-number (1+ (first prime-numbers)) prime-numbers))))
-			((= (mod target-number (first prime-numbers)) 0)
-			 (/ target-number (first prime-numbers)))))
+	(let ((primes '()))
+		(iter (for prime-test from 2 to (- target-number 1))
+					(when (= (mod target-number prime-test) 0)
+						(if (not primes)
+								(setq primes (generate-primes-up-to-number prime-test)))
+						)
+					)))
+
+
+; Third attempt:
+
+;(defun generate-next-prime-number (current-prime-test prime-numbers)
+;	(let ((prime-number))
+;		(iter (for prime-number in prime-numbers)
+;					(cond ((not (= (mod current-prime-test prime-number) 0))
+;								 current-prime-test)
+;								((generate-next-prime-number (1+ current-prime-test) prime-numbers))
+;								))))
+;
+;(defun locate-largest-prime-factor (target-number)
+;	(do ((prime-numbers
+;				'(2)
+;				(push prime-numbers (generate-next-prime-number (1+ (first prime-numbers)) prime-numbers))))
+;			((= (mod target-number (first prime-numbers)) 0)
+;			 (/ target-number (first prime-numbers)))))
 
 ; Second attempt - trying to stuff this problem into the do construct - unsuccessful
 
